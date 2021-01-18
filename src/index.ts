1b07afa99847dde238069a8dcba0672320a2df74
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-01-18 13:42:17 -0800

    first two endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,70 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
+
+import fetch from 'node-fetch'
 
 dotenv.config()
 
 const PORT = process.env.PORT
 
 const app = express()
+const prisma = new PrismaClient()
 
 app.get('/', (req, res) => res.send('You just made an API call to the API API 🤯'))
 app.get('/status', (req, res) => res.send('Operational: ' + Date()))
 app.get('/db', (req, res) => res.send(process.env.DATABASE_URL))
-app.get('/init', async (req, res) => {
-    const prisma = new PrismaClient()
 
-    prisma.api.create({
-        data: {
-            name: 'Space X API',
-            description: 'The Space X API',
-            documentation: 'https://docs.spacexdata.com/',
-            creator_id: 'theswerd'
-        }
-    }).catch((error: any) => {
-        res.status(400).send(error)
-    }).then((dbRes: any) => {
-        res.status(200).send(dbRes)
-    })
+app.post('/api', (req, res) => {
+    if (req.headers.authorization === undefined) {
+        res.status(400).send('No Authorization Header - No Dice 🚫🎲')
+    } else {
+        fetch('https://api.github.com/user', {
+            headers: {
+                Authorization: 'token b047f4352fa4a412e6bd7d27a38e9824db7bd867',
+                Accept: 'application/vnd.github.v3+json'
+            }
+        }).then(async (dbRes): Promise<void> => {
+            if (dbRes.status !== 200) {
+                const user = await dbRes.json()
+                const creatorId: string = user.login
 
+                const name: string = req.body.name
+                const description: string = req.body.description
+                const homepage: string = req.body.homepage
+                const documentation: string = req.body.documentation
+
+                const data = await prisma.api.create({
+                    data: {
+                        name,
+                        description,
+                        homepage,
+                        documentation,
+                        creator_id: creatorId,
+                    }
+                })
+
+                res.send(data)
+
+                // const name: string = user.name
+                // const bio: string =  user.bio
+            } else {
+                res.status(401).send('Failed to Authenticate with Github')
+            }
+
+        }).catch((e) => {
+            res.status(401).send(e)
+        })
+    }
 })
+
+app.get('api/:apiid', async (req, res)=>{
+    const api = await prisma.api.findFirst({
+        where: {
+            id: req.params.apiid as unknown as number
+        }
+    })
+    res.send(api)
+})
+
 app.listen(PORT, () => console.log('Started on port', PORT))
