89e7a3d1ba3d75ad0d9c8f83da50160ab51aab96
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-01-17 22:25:09 -0800

    ah

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,28 @@ dotenv.config()
 const PORT = process.env.PORT
 
 const app = express()
-const prisma = new PrismaClient()
 
 app.get('/', (req, res) => res.send('You just made an API call to the API API 🤯'))
 app.get('/status', (req, res) => res.send('Operational: ' + Date()))
 
 app.get('/init', (req, res) => {
-    prisma.api.create({
-        data: {
-            name: 'Space X API',
-            description: 'The Space X API',
-            documentation: 'https://docs.spacexdata.com/',
-            creator_id: 'theswerd'
-        }
-    }).catch((error: any)=>{
-        res.status(400).send(error)
-    }).then((dbRes: any)=>{
-        res.send(dbRes)
-    })
+    try {
+        const prisma = new PrismaClient()
+
+        prisma.api.create({
+            data: {
+                name: 'Space X API',
+                description: 'The Space X API',
+                documentation: 'https://docs.spacexdata.com/',
+                creator_id: 'theswerd'
+            }
+        }).catch((error: any) => {
+            res.status(400).send(error)
+        }).then((dbRes: any) => {
+            res.send(dbRes)
+        })
+    } catch (error) {
+        res.send(error)
+    }
 })
 app.listen(PORT, () => console.log('Started on port', PORT))
